25c0ef82fbe98d3e41cf93cb26790d50734b6bc7
Daniel Cunha <danielcunha54@gmail.com>
2021-02-28 12:04:28 -0300

    feat: create simple api

---
+++ src/api/index.ts
@@ -0,0 +1,19 @@
+import 'dotenv/config'
+
+import cors from 'cors'
+import express from 'express'
+import helmet from 'helmet'
+import 'express-async-errors'
+
+import { logger, errorHandler } from './middleware'
+import { routes } from './routes'
+
+const app = express()
+app.use(helmet())
+app.use(cors())
+app.use(logger())
+app.use(express.json())
+app.use('/', routes)
+app.use(errorHandler)
+
+export default app
