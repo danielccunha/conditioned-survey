3ef7de8f1a328e70e059a8a3d9e3c891ec4b1bbb
Daniel Cunha <danielcunha54@gmail.com>
2021-02-28 19:30:49 -0300

    test: create Hasher

---
+++ src/services/cryptography/Hasher.spec.ts
@@ -0,0 +1,49 @@
+import bcrypt from 'bcrypt'
+
+import { Hasher } from './Hasher'
+
+jest.mock('bcrypt', () => ({
+  hash: async () => 'hashed_value',
+  compare: async () => true
+}))
+
+const makeSut = () => {
+  const salt = 12
+  const sut = new Hasher(salt)
+  return { sut, salt }
+}
+
+describe('Hasher', () => {
+  test('should call hash with correct values', async () => {
+    const { sut, salt } = makeSut()
+    const hashSpy = jest.spyOn(bcrypt, 'hash')
+    await sut.hash('any_value')
+    expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
+  })
+
+  test('should return a valid hash on success', async () => {
+    const { sut } = makeSut()
+    const hash = await sut.hash('any_value')
+    expect(hash).toBe('hashed_value')
+  })
+
+  test('should call compare with correct values', async () => {
+    const { sut } = makeSut()
+    const compareSpy = jest.spyOn(bcrypt, 'compare')
+    await sut.compare('any_value', 'any_hash')
+    expect(compareSpy).toHaveBeenCalledWith('any_value', 'any_hash')
+  })
+
+  test('should return true when compare succeeds', async () => {
+    const { sut } = makeSut()
+    const isValid = await sut.compare('any_value', 'any_hash')
+    expect(isValid).toBe(true)
+  })
+
+  test('should return false when compare fails', async () => {
+    const { sut } = makeSut()
+    jest.spyOn(bcrypt, 'compare').mockResolvedValueOnce(false)
+    const isValid = await sut.compare('any_value', 'any_hash')
+    expect(isValid).toBe(false)
+  })
+})
