9efbaf8fc204658607923a0b5cae6cec1e3c2862
Daniel Cunha <danielcunha54@gmail.com>
2021-02-28 23:04:06 -0300

    chore: setup jest

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,10 @@
-module.exports = {
+export default {
   clearMocks: true,
   collectCoverageFrom: ['<rootDir>/src/services/*.ts'],
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   testEnvironment: 'node',
   testMatch: ['<rootDir>/**/*.spec.ts'],
+  setupFiles: ['<rootDir>/jest.setup.ts'],
   preset: 'ts-jest'
 }
