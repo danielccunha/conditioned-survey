60e98c323e694d9608489c0e8072092f74c37d5a
Daniel Cunha <danielcunha54@gmail.com>
2021-02-28 22:47:46 -0300

    feat: implement UsersRepository

--- src/database/repositories/UsersRepository.ts
+++ src/database/repositories/UsersRepository.ts
@@ -1,6 +1,24 @@
+import { getRepository, Repository } from 'typeorm'
+
 import { User } from '../entities/User'
 
 export interface UsersRepository {
   findByEmail(email: string): Promise<User>
   store(user: User): Promise<User>
+}
+
+export class UsersRepositoryImpl implements UsersRepository {
+  private repository: Repository<User>
+
+  constructor() {
+    this.repository = getRepository(User)
+  }
+
+  findByEmail(email: string): Promise<User> {
+    return this.repository.findOne({ where: { email } })
+  }
+
+  store(user: User): Promise<User> {
+    return this.repository.save(user)
+  }
 }
